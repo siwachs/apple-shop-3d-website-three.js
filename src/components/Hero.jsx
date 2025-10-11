5c60ab381833e288198757663c6f53a7361f2f30
siwachs <ssiwach.8888@gmail.com>
2025-10-12 00:51:22 +0530

    Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,59 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import useWindowSize from "../hooks/useWindowSize";
+
+import { heroVideo, smallHeroVideo } from "../utils";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  const { width } = useWindowSize();
+
+  useGSAP(() => {
+    gsap.to("#hero-title", {
+      opacity: 1,
+      delay: 2,
+    });
+
+    gsap.to("#call-to-action", {
+      opacity: 1,
+      y: -50,
+      delay: 2,
+    });
+  }, []);
+
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero-title" className="hero-title">
+          iPhone 15 Pro
+        </p>
+
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline
+            key={width < 760 ? smallHeroVideo : heroVideo}
+          >
+            <source
+              src={width < 760 ? smallHeroVideo : heroVideo}
+              type="video/mp4"
+            />
+          </video>
+        </div>
+      </div>
+
+      <div
+        id="call-to-action"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
---
+++ src/hooks/useWindowSize.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const resize = () => {
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+
+    window.addEventListener("resize", resize);
+
+    return () => window.removeEventListener("resize", resize);
+  }, []);
+
+  return windowSize;
+};
+
+export default useWindowSize;
